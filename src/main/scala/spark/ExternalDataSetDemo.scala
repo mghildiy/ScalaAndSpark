603fe52d470ccf8efa9699f28d6d1469f71d91f1
Ghildiyal <manish.ghildiyal@here.com>
2018-01-06 22:50:11 +0530

    Minor changes

--- src/main/scala/spark/ExternalDataSetDemo.scala
+++ src/main/scala/spark/ExternalDataSetDemo.scala
@@ -14,5 +14,26 @@ object ExternalDataSetDemo {
     val logRDD = sc.textFile(inputFile)
     println(logRDD.getClass)
     println(logRDD)
+    println(logRDD.first())
+
+    val lineLengths = logRDD.map(s => s.length)
+    println(lineLengths.getClass)
+    println(lineLengths)
+    //println(lineLengths.count())
+    //lineLengths.foreach(println)
+    //lineLengths.take(100).foreach(println)
+    println(lineLengths.reduce((x,y) => x+y))
+
+    //key value tests
+    val crapRDD = sc.textFile("D:\\projects\\ScalaSpark\\for-program-testing\\crap.txt")
+    println(crapRDD)
+    println(crapRDD.getClass())
+    val lineToOne = crapRDD.map(line => (line,1))
+    println(lineToOne)
+    println(lineToOne.getClass())
+    //print(lineTOOne.first())
+    val lineCounts = lineToOne.reduceByKey((x,y) => x+y)
+    lineCounts.sortByKey()
+    lineCounts.collect().foreach(println)
   }
 }
